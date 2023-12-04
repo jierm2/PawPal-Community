d11c77d3f3c39ff5107d35329fd746a3d719e925
aaminaali <aali220@illinois.edu>
2023-12-04 16:43:07 -0600

    remaining pages

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -1,8 +1,56 @@
+//moved services from homepage onto here
+
 import * as React from "react";
+import "./Services.css"
+import DogIcon from '@mui/icons-material/Pets'; // Example icon
+import StarIcon from '@mui/icons-material/StarRate';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import PeopleIcon from '@mui/icons-material/People';
+
+
+function ServiceItem({ IconComponent, title, description }) {
+    return (
+      <div className="service-item">
+        <div className="service-icon">
+          <IconComponent fontSize="large" />
+        </div>
+        <h3 className="text-amber-400">{title}</h3>
+        <p className="smaller-text">{description}</p>
+      </div>
+    );
+}
 
 function Services(props) {
-    return (    <div className="self-stretch text-amber-400 text-3xl font-medium leading-[58px] mt-5 max-md:max-w-full">
-    <span className="text-white"> Services, still developing...</span></div>);
+    const services = [
+        { IconComponent: DogIcon, title: "Dog Walking Services", description: "Connect with trustworthy dog walkers in your community. Schedule walks, track your dog’s activity, and ensure your pet is in safe hands." },
+        { IconComponent: StarIcon, title: "Dog Sitting Exchange", description: "Find reliable sitters for your dog while you're away, or become a sitter yourself to earn credits and spend time with amazing pets." },
+        { IconComponent: PeopleIcon, title: "Community Connections", description: "Join a passionate community of dog lovers. Share stories, get advice, and form lasting friendships with other pet owners." },
+        { IconComponent: CreditCardIcon, title: "Credit System", description: "Participate in our unique credit system. Earn credits by providing services and spend them on care for your pets or at local pet-friendly businesses." },
+        { IconComponent: StarIcon, title: "Reviews and Ratings", description: "Build trust within the community with a transparent review and rating system for walkers and sitters." },
+        { IconComponent: StoreIcon, title: "Support for Local Pet Businesses", description: "Discover and support local businesses. Use your credits for discounts on goods and services that make your pet's life better." }
+      ];
+    return (    
+    <div className="self-stretch text-amber-400 text-3xl font-medium leading-[58px] mt-5 max-md:max-w-full">
+    <div>
+      <div className="text-center text-amber-400 text-4xl font-medium leading-normal mt-20 mb-12">
+        <span className="text-white">Our </span>
+        <span>Services</span>
+      </div>
+      <div className="services-container text-white ">
+        {services.map((service, index) => (
+          <ServiceItem
+            key={index}
+            IconComponent={service.IconComponent}
+            title={service.title}
+            description={service.description}
+          />
+        ))}
+      </div>
+    </div>
+    
+    </div>
+    );
 
 
 
--- frontend/src/pages/WalkerPage/Walker.js
+++ frontend/src/pages/WalkerPage/Walker.js
@@ -1,13 +1,150 @@
-import React from 'react';
-// import Button from '@mui/material/Button';
-// import TextField from '@mui/material/TextField';
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 // import { Link } from 'react-router-dom';
+import "./Walker.css"
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#fdd835', 
+    },
+  },
+  components: {
+    // Name of the component
+    MuiTextField: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          input: {
+            color: 'white', // Text color
+          },
+          '& label': {
+            color: 'gray', // Label color
+          },
+          '& label.Mui-focused': {
+            color: 'white', // Label color when the input is focused
+          },
+          '& .MuiInput-underline:before': {
+            borderBottomColor: 'gray', // Underline color before input is touched
+          },
+          '& .MuiInput-underline:after': {
+            borderBottomColor: 'white', // Underline color when input is focused
+          },
+        },
+      },
+    },
+  },
+});
+
+function DropdownExample() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <label className="text-gray-300 text-center mb-8" htmlFor="number">How many dogs?</label>
+      <br></br>
+            <select className=" custom-dropdown text-white text-center mb-8" id="number" value={selectedOption} onChange={handleDropdownChange}>
+        <option value="">-- Please Select Amount --</option>
+        <option value="One dog">1</option>
+        <option value="Two dogs">2</option>
+        <option value="Three dogs">3</option>
+        <option value="Four dogs">4</option>
+      </select>
+
+      {/* <p className="text-gray-300 text-center mb-8">Selected Number: {selectedOption}</p> */}
+    </div>
+  );
+}
+
+
+function DropdownExample2() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <label className="text-gray-300 text-center mb-8" htmlFor="weight">Dog Weight (lbs)</label>
+      <br></br>
+      <select className=" custom-dropdown text-white text-center mb-8" id="weight" value={selectedOption} onChange={handleDropdownChange}>
+        <option value="">-- Please Select Weight --</option>
+        <option value="1-15">1 - 15</option>
+        <option value="16">16 - 30</option>
+        <option value="31">31 - 50</option>
+        <option value="51">51- 70</option>
+        <option value="71">71 - 100</option>
+        <option value="100">100+</option>
+      </select>
+
+      {/* <p className="text-gray-300 text-center mb-8">Selected Number: {selectedOption}</p> */}
+    </div>
+  );
+}
+
 
 function Walker() {
   return (
-    <div className="self-stretch text-amber-400 text-3xl font-medium leading-[58px] mt-5 max-md:max-w-full">
-    <span className="text-white"> Dog walker, still developing...</span></div>
+    // <div className="self-stretch text-amber-400 text-3xl font-medium leading-[58px] mt-5 max-md:max-w-full">
+    <ThemeProvider theme={theme}>
+      <h2 className="text-yellow-400 text-4xl font-bold mb-6 text-center">Find The Perfect Match For Your Dog's Needs</h2>
+    <div className="min-h-[80vh] flex items-center justify-center">
+        <div className="p-1 rounded-lg shadow-xl text-white max-w-lg w-full">
+        <p className="text-gray-300 text-center mb-8">
+          Whether you have a busy schedule, unexpected plans, or just need an extra hand in caring for your furry friend, 
+          we're here to help you connect with reliable and loving dog walkers in your area. Our dedicated dog walkers are 
+          passionate about ensuring the well-being of your pets and are ready to provide the care and attention they deserve.
+          Start your search now and discover the ideal dog walker who will treat your pet like family!
+          </p>
+          <form className="space-y-4">
+            <div className="row">
+              <TextField
+                fullWidth
+                label="Enter Zip Code"
+                variant="filled"
+                margin="normal"
+                style={{ flex: 1, marginRight: '8px' }}
+              />
+              <TextField
+                fullWidth
+                label="Enter Date for Sitting"
+                variant="filled"
+                margin="normal"
+                style={{ flex: 1, marginRight: '8px' }}
+              />
+              <TextField
+                fullWidth
+                label="Enter Time Range for Sitting (hours)"
+                variant="filled"
+                margin="normal"
+                style={{ flex: 1, marginRight: '8px' }}
+              />
+            </div>
+            <DropdownExample /> 
+            <DropdownExample2 /> 
+            <Button
+              type="Search"
+              fullWidth
+              variant="contained"
+              color="primary"
+              size="large"
+              style={{ marginTop: '16px' }}
+            >
+              Search
+            </Button>
+          </form>
+        </div>
+      </div>
+      </ThemeProvider>
+    // </div>
     );
 }
 
