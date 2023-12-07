e000256b3f841b2e7f15959bb27712d98269cb3a
Jason Barnett <jasobar627@gmail.com>
2023-12-07 17:41:20 -0600

    added signup and sign in user auth

--- frontend/src/pages/LoginPage/Login.js
+++ frontend/src/pages/LoginPage/Login.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../../secrets/firebase-secrets";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#fdd835', // Your brand color for buttons and such
+      main: "#fdd835", // Your brand color for buttons and such
     },
     white: {
-      main:'#FFFFFF'
-    }
+      main: "#FFFFFF",
+    },
   },
   components: {
     // Name of the component
@@ -23,19 +26,19 @@ const theme = createTheme({
         root: {
           // Some CSS
           input: {
-            color: 'white', // Text color
+            color: "white", // Text color
           },
-          '& label': {
-            color: 'gray', // Label color
+          "& label": {
+            color: "gray", // Label color
           },
-          '& label.Mui-focused': {
-            color: 'white', // Label color when the input is focused
+          "& label.Mui-focused": {
+            color: "white", // Label color when the input is focused
           },
-          '& .MuiInput-underline:before': {
-            borderBottomColor: 'gray', // Underline color before input is touched
+          "& .MuiInput-underline:before": {
+            borderBottomColor: "gray", // Underline color before input is touched
           },
-          '& .MuiInput-underline:after': {
-            borderBottomColor: 'white', // Underline color when input is focused
+          "& .MuiInput-underline:after": {
+            borderBottomColor: "white", // Underline color when input is focused
           },
         },
       },
@@ -44,17 +47,32 @@ const theme = createTheme({
 });
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <div className="min-h-[80vh]  flex items-center justify-center">
         <div className="p-1 rounded-lg shadow-xl text-white max-w-lg w-full">
-          <h2 className="text-yellow-400 text-4xl font-bold mb-6 text-center">Log in</h2>
+          <h2 className="text-yellow-400 text-4xl font-bold mb-6 text-center">
+            Log in
+          </h2>
           <p className="text-gray-300 text-center mb-8">
-          Welcome back! Please log in to access your account.          </p>
+            Welcome back! Please log in to access your account.{" "}
+          </p>
           <form className="space-y-4">
-            <div className="row">
-              
-            </div>
+            <div className="row"></div>
             <div className="row">
               <TextField
                 fullWidth
@@ -62,7 +80,8 @@ function Login() {
                 variant="filled"
                 margin="normal"
                 type="email"
-                style={{ flex: 1, marginRight: '8px' }} // Add some right margin to the first field
+                style={{ flex: 1, marginRight: "8px" }} // Add some right margin to the first field
+                onChange={(e) => setEmail(e.target.value)}
               />
               <TextField
                 fullWidth
@@ -71,12 +90,12 @@ function Login() {
                 margin="normal"
                 type="password"
                 style={{ flex: 1 }}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="text-center mt-4">
               <Button color="white" component={Link} to="/sign-up">
-             Forgot Password?
-
+                Forgot Password?
               </Button>
             </div>
             <Button
@@ -85,15 +104,15 @@ function Login() {
               variant="contained"
               color="primary"
               size="large"
-              style={{ marginTop: '16px' }}
+              style={{ marginTop: "16px" }}
+              onClick={signIn}
             >
-        Login            
-        </Button>
-            
+              Login
+            </Button>
+
             <div className="text-center mt-4">
               <Button color="white" component={Link} to="/sign-up">
-              Don't have an account? Sign Up
-
+                Don't have an account? Sign Up
               </Button>
             </div>
           </form>
--- frontend/src/pages/SignupPage/Signup.js
+++ frontend/src/pages/SignupPage/Signup.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import { auth } from "../../secrets/firebase-secrets";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#fdd835', 
+      main: "#fdd835",
     },
   },
   components: {
@@ -18,19 +21,19 @@ const theme = createTheme({
         // Name of the slot
         root: {
           input: {
-            color: 'white', // Text color
+            color: "white", // Text color
           },
-          '& label': {
-            color: 'gray', // Label color
+          "& label": {
+            color: "gray", // Label color
           },
-          '& label.Mui-focused': {
-            color: 'white', // Label color when the input is focused
+          "& label.Mui-focused": {
+            color: "white", // Label color when the input is focused
           },
-          '& .MuiInput-underline:before': {
-            borderBottomColor: 'gray', // Underline color before input is touched
+          "& .MuiInput-underline:before": {
+            borderBottomColor: "gray", // Underline color before input is touched
           },
-          '& .MuiInput-underline:after': {
-            borderBottomColor: 'white', // Underline color when input is focused
+          "& .MuiInput-underline:after": {
+            borderBottomColor: "white", // Underline color when input is focused
           },
         },
       },
@@ -39,13 +42,37 @@ const theme = createTheme({
 });
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === "auth/email-already-in-use") {
+          // email already exists
+        } else if (errorCode === "auth/invalid-email") {
+          // invalid email
+        }
+      });
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <div className="min-h-[80vh] flex items-center justify-center">
         <div className="p-1 rounded-lg shadow-xl text-white max-w-lg w-full">
-          <h2 className="text-yellow-400 text-4xl font-bold mb-6 text-center">Sign Up</h2>
+          <h2 className="text-yellow-400 text-4xl font-bold mb-6 text-center">
+            Sign Up
+          </h2>
           <p className="text-gray-300 text-center mb-8">
-            Join our community today! Create an account to unlock exclusive features and personalized experiences.
+            Join our community today! Create an account to unlock exclusive
+            features and personalized experiences.
           </p>
           <form className="space-y-4">
             <div className="row">
@@ -54,7 +81,8 @@ function Signup() {
                 label="Enter First Name"
                 variant="filled"
                 margin="normal"
-                style={{ flex: 1, marginRight: '8px' }}
+                style={{ flex: 1, marginRight: "8px" }}
+                onChange={(e) => setFirstName(e.target.value)}
               />
               <TextField
                 fullWidth
@@ -62,6 +90,7 @@ function Signup() {
                 variant="filled"
                 margin="normal"
                 style={{ flex: 1 }}
+                onChange={(e) => setLastName(e.target.value)}
               />
             </div>
             <div className="row">
@@ -71,7 +100,8 @@ function Signup() {
                 variant="filled"
                 margin="normal"
                 type="email"
-                style={{ flex: 1, marginRight: '8px' }} 
+                style={{ flex: 1, marginRight: "8px" }}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <TextField
                 fullWidth
@@ -80,6 +110,7 @@ function Signup() {
                 margin="normal"
                 type="password"
                 style={{ flex: 1 }}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <Button
@@ -88,7 +119,7 @@ function Signup() {
               variant="contained"
               color="primary"
               size="large"
-              style={{ marginTop: '16px' }}
+              style={{ marginTop: "16px" }}
             >
               Sign Up
             </Button>
