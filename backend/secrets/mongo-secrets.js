5488f00f1dade2952e1451dfef2afbca31924f0a
Jimmy Miao <jierm2@illinois.edu>
2024-05-29 22:58:07 -0400

    Update mongo-secrets.js

--- backend/secrets/mongo-secrets.js
+++ backend/secrets/mongo-secrets.js
@@ -1,5 +1,5 @@
 const mongoSecrets = {
-  mongo_connection: "mongodb+srv://admin:L1jUqeCg5KvakpDe@cluster0.xomh02u.mongodb.net/?retryWrites=true&w=majority",
+  mongo_connection: "",
 };
 
 module.exports = mongoSecrets;
